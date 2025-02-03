5bee99ce961b1f6bf0cb5d1aab4e293a139193da
Nico Müller <muellerweb@gmx.de>
2025-02-03 15:10:52 +0100

    Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+print(np.__version__)
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+arr = np.array([1, 2, 3, 4, 5, "jasdk"])
+
+print(arr)
+
+arr = np.array([[1, 2, 3, 4, 5],[63 , 473, 828]])
+
+print(arr)
+
+print(type(arr))
+
+print(np.__version__)
+
